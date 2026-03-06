82096113b963d6de84fe1f4c71b57c6519795f2b
NicoleKerschner08 <nicole.kerschner@htlwy.at>
2026-03-06 11:36:33 +0100

    21_calcWPF added

---
+++ 21_calcWPF/21_calcWPF/numberModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace _21_calcWPF
+{
+    internal class numberModel
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public int result { get; set; } = 0;
+        public string staus { get; set; } = "";
+        public SolidColorBrush brush { get; set; } = Brushes.Black;
+    }
+}
---
+++ 21_calcWPF/21_calcWPF/numberViewModel.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace _21_calcWPF
+{
+    internal class numberViewModel : ICommand, INotifyPropertyChanged
+    {
+        private numberModel model = new numberModel();
+        public ICommand cmdButtonClick { get; set; }
+        public event EventHandler CanExecuteChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int number1
+        {
+            get { return model.number1; }
+            set
+            {
+                model.number1 = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(number1)));
+            }
+
+        }
+
+        public int number2
+        {
+            get { return model.number2; }
+            set
+            {
+                model.number2 = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(number2)));
+            }
+        }
+
+        public int result { 
+            get { return model.result; } 
+            set 
+            {
+                model.result = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(result)));
+            }
+        }
+
+        public string status
+        {
+            get { return model.staus; }
+            set
+            {
+                model.staus = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(status)));
+            }
+        }
+
+        public SolidColorBrush brush
+        {
+            get { return model.brush; }
+            set
+            {
+                model.brush = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(brush)));
+
+            }
+        }
+
+        public numberViewModel()
+        {
+            cmdButtonClick = this;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            try
+            {
+
+                if((string)parameter == "addition")
+                {
+                    result = number1 + number2;
+                    status ="Addition erfolgreich";
+                }
+                 else if ((string)parameter == "subtraktion")
+                {
+                    result = number1 - number2;
+                    status = "Subtraktion erfolgreich";
+                }
+                 else if ((string)parameter == "multiplikation")
+                {
+                    result = number1 * number2;
+                    status = "Multiplikation erfolgreich";
+                }
+                 else if ((string)parameter == "division")
+                {
+                    result = number1 / number2;
+                    status = "Division erfolgreich";
+                }
+                else
+                {
+                    result = 0;
+                    number1 = 0;
+                    number2 = 0;
+                    brush = new SolidColorBrush(Colors.Black);  
+                }
+
+                if(result < 0)
+                {
+                    brush = new SolidColorBrush(Colors.Red);
+                }
+                else if(result > 0)
+                {
+                    brush = new SolidColorBrush(Colors.LightGreen);
+                }
+                else
+                {
+                    brush = new SolidColorBrush(Colors.Black);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                status = ex.Message;
+            }
+        }
+    }
+}
