112cb3be69be0f242ca6a60338f1a5e02dbb0186
NicoleKerschner08 <nicole.kerschner@htlwy.at>
2026-04-01 15:19:26 +0200

    23_click_counter added

---
+++ 23_Click_Counter/23_Click_Counter/LogEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Click_Counter
+{
+    internal class LogEntry
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+
+        public override string ToString()
+        {
+            return "Klick#"+Id + " " + TimeStamp.ToString("HH:mm:ss");
+        }
+    }
+}
---
+++ 23_Click_Counter/23_Click_Counter/ViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _23_Click_Counter
+{
+    internal class ViewModel: INotifyCollectionChanged, ICommand, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler CanExecuteChanged;
+        private ObservableCollection<LogEntry> collection = new ObservableCollection<LogEntry>();
+        public ICommand cmdClick { get; }
+
+        public ViewModel() { 
+            cmdClick = this;
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged
+        {
+            add
+            {
+                ((INotifyCollectionChanged)Collection).CollectionChanged += value;
+            }
+
+            remove
+            {
+                ((INotifyCollectionChanged)Collection).CollectionChanged -= value;
+            }
+        }
+
+        public ObservableCollection<LogEntry> Collection
+        {
+            get { return collection; }
+            set
+            {
+                collection = value;
+            }
+        }
+
+        public string count { get { return "Gesamtanzahl der Klicks: "+ collection.Count; } }
+
+        public bool CanExecute(object parameter)
+        {
+            if ((string)parameter == "clear")
+            {
+                if (collection.Count > 0)
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if((string)parameter == "log")
+            {
+                LogEntry entry = new LogEntry();
+                entry.Id = collection.Count + 1;
+                collection.Add(entry);
+            }
+            else if((string)parameter == "clear")
+            {
+                collection.Clear();
+            }
+            else { }
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(count)));
+            CanExecuteChanged.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
